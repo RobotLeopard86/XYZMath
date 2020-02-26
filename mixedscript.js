b79735328e5e04cd8911e3e7c11855f292ac5c7a
Owen Siebers <RobotLeopard86>
2020-02-25 19:57:08 -0600

    Consolidated play modes.

--- mixedscript.js
+++ mixedscript.js
@@ -1,79 +1,152 @@
-function chooseOperation(){
-	return Math.floor(Math.random() * 4);
+var pages = [document.getElementById("settingspage"), document.getElementById("gamepage")];
+var questionsToDo;
+var answerBox = document.getElementById("answerBox");
+var selectOperationBoxes = [document.getElementById("aselect"), document.getElementById("sselect"), document.getElementById("mselect"), document.getElementById("dselect")];
+var operations = ["a", "s", "m", "d"];
+var timerlengthbox = document.getElementById("timervalue");
+var timerText = document.getElementById("timerText");
+var timerValue;
+var timerId;
+var timerZone = document.getElementById("timerContents");
+var questionCount = 0;
+var firstNum;
+var secondNum;
+var realAnswer;
+var min;
+var max;
+var timerlength;
+
+function parseSettings(){
+	usedOperations = [];
+	for(var j = 0; j < selectOperationBoxes.length; j++){
+		if(selectOperationBoxes[j].checked){
+			usedOperations.push(operations[j]);
+		}
+	}
+	min = parseInt(document.getElementById("min").value);
+	max = parseInt(document.getElementById("max").value);
+	timerlength = parseInt(timerlengthbox.value);
+	questionsToDo = parseInt(document.getElementById("ques").value);
+	if(usedOperations.length == 0){
+		alert("You must select at least one operation.");
+		return;
+	}
+	if(isNaN(min) || isNaN(max) || isNaN(questionsToDo) || isNaN(timerlength)){
+		alert("Please enter a valid number.");
+		return;
+	}
+	if(timerlength < 0){
+		alert("Your timer cannot be less than 0.");
+		return;
+	}
+	if(questionsToDo <= 0){
+		alert("You must input a question count of 1 or above to start a round.");
+		return;
+	}
+	if(min >= max){
+		alert("Please set the minimum value to be less than the maximum value.");
+		return;
+	}
+	if(timerlength == 0){
+		if(confirm("Timer is off for this round. Press OK to edit timer value.")){
+			return;
+		}
+	}
+	pages[0].style.display = "none";
+	pages[1].style.display = "block";
 }
 
 function getRandom(){
-	return Math.floor(Math.random() * 13);
+	return min + Math.floor(Math.random() * (max - min));
 }
 
-var firstNum = getRandom();
-var secondNum = getRandom();
-var realAnswer;
-var symbol = document.getElementById("symbol");
-var title = document.getElementById("title");
-var operation = chooseOperation();
-var message = document.getElementById("message");
-
-function setupAddition(){
-	realAnswer = firstNum + secondNum;
-	symbol.innerHTML = "+";
-	title.innerHTML = "What is the sum of:";
-	document.getElementById("firstdigit").innerHTML = firstNum;
-	document.getElementById("seconddigit").innerHTML = secondNum;
+function chooseOperation(){
+	 return usedOperations[Math.floor(Math.random() * usedOperations.length)];
 }
 
-function setupSubtraction(){
-	realAnswer = firstNum - secondNum;
-	symbol.innerHTML = "-";
-	title.innerHTML = "What is the difference of:";
-	document.getElementById("firstdigit").innerHTML = firstNum;
-	document.getElementById("seconddigit").innerHTML = secondNum;
+function setupQuestion(){
+	questionCount++;
+	if(questionCount > questionsToDo){
+		endRound();
+		return;
+	}
+	var chosenOperation = chooseOperation();
+	firstNum = getRandom();
+	secondNum = getRandom();
+	switch(chosenOperation){
+		case "a":
+			symbol.innerHTML = "+";
+			realAnswer = firstNum + secondNum;
+			firstDigit.innerHTML = firstNum;
+			secondDigit.innerHTML = secondNum;
+			break;
+		case "s":
+			symbol.innerHTML = "-";
+			realAnswer = firstNum - secondNum;
+			firstDigit.innerHTML = firstNum;
+			secondDigit.innerHTML = secondNum;
+			break;
+		case "m":
+			symbol.innerHTML = "x";
+			realAnswer = firstNum * secondNum;
+			firstDigit.innerHTML = firstNum;
+			secondDigit.innerHTML = secondNum;
+			break;
+		case "d":
+			symbol.innerHTML = "&#247;";
+			var dividend = firstNum * secondNum;
+			realAnswer = firstNum;
+			firstDigit.innerHTML = dividend;
+			secondDigit.innerHTML = secondNum;
+			break;
+	}
+	if(timerlength != 0){
+		timerValue = timerlengthbox.value;
+		timerId = setInterval(checkTimer, 100);
+	}
+	answerBox.value = "";
 }
 
-function setupMultiplication(){
-	realAnswer = firstNum * secondNum;
-	symbol.innerHTML = "x";
-	title.innerHTML = "What is the product of:";
-	document.getElementById("firstdigit").innerHTML = firstNum;
-	document.getElementById("seconddigit").innerHTML = secondNum;
-}
+var answers = [];
 
-function setupDivision(){
-	var dividend = firstNum * secondNum;
-	realAnswer = firstNum;
-	symbol.innerHTML = "&#247;";
-	title.innerHTML = "What is the quotient of:";
-	document.getElementById("firstdigit").innerHTML = dividend;
-	document.getElementById("seconddigit").innerHTML = secondNum;
-}
-
-switch(operation){
-	case 0:
-		setupAddition();
-		break;
-	case 1:
-		setupSubtraction();
-		break;
-	case 2:
-		setupMultiplication();
-		break;
-	case 3:
-		setupDivision();
-		break;
+function checkTimer(){
+	timerValue = timerValue - 0.1;
+	timerText.innerHTML = Math.ceil(timerValue) + " seconds remaining.";
+	if(timerValue <= 0){
+		answers.push("unanswered");
+		clearInterval(timerId);
+		setupQuestion();
+	}
 }
 
 function checkAnswer(){
-	var yourAnswer = document.getElementById("answer").value;
+	var yourAnswer = answerBox.value;
 	if(realAnswer == yourAnswer){
-		message.innerHTML = "Correct! Good job!";
+		answers.push("correct")
 	} else {
-		message.innerHTML = "Incorrect. Please try again.";
+		answers.push("incorrect");
 	}
-	document.getElementById("answer").value = "";
+	clearInterval(timerId);
+	setupQuestion();
 }
 
 function checkKey(){
 	if(event.key == "Enter"){
 		checkAnswer();
 	}
+}
+
+function startRound(){
+	setupQuestion();
+}
+
+function endRound(){
+	var score = 0;
+	for(var i = 0; i < answers.length; i++){
+		if(answers[i] == "correct"){
+			score++;
+		}
+	}
+	alert("You got " + score + " out of " + questionsToDo + " questions correct!");
+	window.open("modeselect.html", "_self");
 }
